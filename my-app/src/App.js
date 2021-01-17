9fdac28b1938c28b6009e439d3c759872561c66b
DianaDee <dduong013@gmail.com>
2021-01-17 16:52:11 -0600

    add props and states

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,44 @@ import React, {Component} from 'react'
 import Table from './Table'
 
 class App extends Component {
-    render() {
+    state = { // stores data and allows updates
+        characters: [
+            {
+                name: 'Charlie',
+                job: 'Janitor',
+            },
+            {
+                name: 'Mac',
+                job: 'Bouncer',
+            },
+            {
+                name: 'Dee',
+                job: 'Aspring actress',
+            },
+            {
+                name: 'Dennis',
+                job: 'Bartender',
+            }
+        ],
+    }
+
+    deleteCharacter = (index) => {
+        const {characters} = this.state
+      
+        this.setState({ //updates characters 
+          characters: characters.filter((character, i) => { //filtering array based on index
+            return i !== index
+          }),
+        })
+      }
+
+      render() {
+        const { characters } = this.state
+
+
       return (
         <div className="container">
-          <Table />
+          <Table characterData={characters} deleteCharacter={this.deleteCharacter}/>
         </div>
       )
     }
